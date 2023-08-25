766a819b2db7d16970a74d0c43e7d3432b60300a
sydneydefelice <sdefelice3@gatech.edu>
2023-08-25 12:24:54 -0400

    Pushing empty project files

---
+++ aac-ios/aac-ios/aac_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  aac_iosApp.swift
+//  aac-ios
+//
+//  Created by Sydney DeFelice on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct aac_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

917eea6a2eff1e749d2413788bd8b52ad25d0a6d
sydneydefelice <sdefelice3@gatech.edu>
2023-10-13 14:23:20 -0400

    initial commits for main screen refactoring

--- aac-ios/aac-ios/MainScreen.swift
+++ aac-ios/aac-ios/MainScreen.swift
@@ -12,33 +12,31 @@ import SwiftUI
 struct MainScreen: View {
     @State private var selectedButton: String? = nil
     var body: some View {
-        HorizontalNavBar()
-        NavigationView {
-            VStack {
-                VStack {
-                    NavigationLink(destination: SceneDisplay(), tag: "Images", selection: $selectedButton) {
-                        NavigationButton(labelText: "Images", image: "eye")
+        HStack {
+            NavigationView {
+                    VStack {
+                        NavigationLink(destination: SceneDisplay(), tag: "Images", selection: $selectedButton) {
+                            NavigationButton(labelText: "Images", image: "eye")
+                        }
+                        .buttonStyle(CustomButtonStyle(selected: selectedButton == "Images"))
+                        
+                        NavigationLink(destination: WhiteBoard(), tag: "WhiteBoard", selection: $selectedButton) {
+                            NavigationButton(labelText: "White Board", image: "hand.draw")
+                        }
+                        .buttonStyle(CustomButtonStyle(selected: selectedButton == "WhiteBoard"))
+                        
+                        NavigationLink(destination: Build(), tag: "Build", selection: $selectedButton) {
+                            NavigationButton(labelText: "Build", image: "pencil.line")
+                        }
+                        .buttonStyle(CustomButtonStyle(selected: selectedButton == "Build"))
+                        
+                        NavigationLink(destination: Scripts(), tag: "Scripts", selection: $selectedButton) {
+                            NavigationButton(labelText: "Scripts", image: "scroll")
+                        }
+                        .buttonStyle(CustomButtonStyle(selected: selectedButton == "Scripts"))
+                        AddButton()
                     }
-                    .buttonStyle(CustomButtonStyle(selected: selectedButton == "Images"))
-                    
-                    NavigationLink(destination: WhiteBoard(), tag: "WhiteBoard", selection: $selectedButton) {
-                        NavigationButton(labelText: "White Board", image: "hand.draw")
-                    }
-                    .buttonStyle(CustomButtonStyle(selected: selectedButton == "WhiteBoard"))
-                    
-                    NavigationLink(destination: Build(), tag: "Build", selection: $selectedButton) {
-                        NavigationButton(labelText: "Build", image: "pencil.line")
-                    }
-                    .buttonStyle(CustomButtonStyle(selected: selectedButton == "Build"))
-                    
-                    NavigationLink(destination: Scripts(), tag: "Scripts", selection: $selectedButton) {
-                        NavigationButton(labelText: "Scripts", image: "scroll")
-                    }
-                    .buttonStyle(CustomButtonStyle(selected: selectedButton == "Scripts"))
-                    AddButton()
-                }
-                .navigationBarHidden(true)
-                
+                    .navigationBarHidden(true)
             }
         }
     }
--- aac-ios/aac-ios/UI/Views/SceneDisplay.swift
+++ aac-ios/aac-ios/UI/Views/SceneDisplay.swift
@@ -9,6 +9,17 @@ import SwiftUI
 
 struct SceneDisplay: View {
     var body: some View {
-        Text("Scene Display Screen")
+        VStack {
+            HorizontalNavBar()
+            Text("Scene Display Screen")
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+    }
+}
+
+struct SceneDisplay_Previews: PreviewProvider {
+    static var previews: some View {
+        SceneDisplay()
+            .previewInterfaceOrientation(.landscapeLeft)
     }
 }

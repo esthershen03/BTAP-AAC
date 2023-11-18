dd1d5221c5968c0980be582b91e84bc704ed0884
qye47 <yeqqww5109@gmail.com>
2023-11-18 18:02:24 -0500

    white board function

---
+++ aac-ios/aac-ios/UI/Views/whiteBoard/ColorPickerView.swift
@@ -0,0 +1,47 @@
+//
+//  ColorPickerView.swift
+//  aac-ios
+//
+//  Created by Joannaye on 2023/11/18.
+//
+
+import SwiftUI
+
+struct ColorPickerView: View {
+    
+    let colors = [Color.red, Color.orange, Color.green, Color.blue, Color.purple]
+    @Binding var selectedColor: Color
+    
+    var body: some View {
+        HStack {
+            ForEach(colors, id: \.self) { color in
+            
+                Image(systemName: selectedColor == color ? Icons.recordCircleFill : Icons.circleFill)
+                    .foregroundColor(color)
+                    .font(.system(size: 20))
+                    .clipShape(Circle())
+                    .onTapGesture {
+                        selectedColor = color
+                    }
+            }
+        }
+        
+    }
+}
+
+struct ColorListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorPickerView(selectedColor: .constant(.blue))
+    }
+}
+
+struct Icons {
+    static let plusCircle = "plus.circle"
+    static let line3HorizontalCircleFill = "line.3.horizontal.circle.fill"
+    static let circle = "circle"
+    static let circleInsetFilled = "circle.inset.filled"
+    static let exclaimationMarkCircle = "exclamationmark.circle"
+    static let recordCircleFill = "record.circle.fill"
+    static let trayCircleFill = "tray.circle.fill"
+    static let circleFill = "circle.fill"
+}
---
+++ aac-ios/aac-ios/UI/Views/whiteBoard/DrawingEngine.swift
@@ -0,0 +1,18 @@
+//
+//  DrawingEngine.swift
+//  aac-ios
+//
+//  Created by Joannaye on 2023/11/18.
+//
+
+import SwiftUI
+
+struct DrawingEngine: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DrawingEngine()
+}

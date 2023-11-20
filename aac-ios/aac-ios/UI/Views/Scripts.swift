dbd76bdb2a77833c63368f0748ffb5f973477093
Wu, Emily Jaling <ewu64@gatech.edu>
2023-11-20 14:37:42 -0500

    create grid layout for script

--- aac-ios/aac-ios/UI/Views/Scripts.swift
+++ aac-ios/aac-ios/UI/Views/Scripts.swift
@@ -2,19 +2,49 @@
 //  Scripts.swift
 //  aac-ios
 //
-//  Created by Sydney DeFelice on 10/4/23.
+//  Created by Emily Wu on 10/04/23.
 //
 
 import SwiftUI
+struct ScriptTile: Identifiable {
+    var id: ObjectIdentifier?
+    var label: String
+    var image: String
+    var sentences: [String]?
+}
 
 struct Scripts: View {
+    let labels = ["Health", "Fitness", "Movies", "Sports",
+                  "Travel", "Greetings", "Shopping", "Social", "Favors",
+                  "Invites", "Music", "Aid", "Money", "Clothes",
+                  "TV Shows", "Work"]
+    
+    @State private var tiles: [ScriptTile] = []
+    private let adaptiveColumns = [
+        GridItem(.adaptive(minimum: 300))
+    ]
+    
     var body: some View {
-        VStack() {
-            Text("Scripts Screen")
+        LazyVGrid(columns: adaptiveColumns, spacing: 40) {
+            ForEach(tiles, id: \.label) { tile in
+                            GridTile(labelText: tile.label, image: tile.image)
+            }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .padding(.bottom, -21)
         .navigationBarHidden(true)
+        .onAppear {
+            self.tiles = self.createTiles()
+        }
+    }
+    
+    func createTiles() -> [ScriptTile] {
+        var result: [ScriptTile] = []
+        for label in labels {
+            let tile = ScriptTile(label: label, image: "eye")
+            result.append(tile)
+        }
+        return result
     }
 }
 

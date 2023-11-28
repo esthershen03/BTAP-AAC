a5ebd4fd67f0ae68cab919dffffb2c8b740674a6
qye47 <yeqqww5109@gmail.com>
2023-11-28 16:57:37 -0500

    Add signup screen

---
+++ aac-ios/aac-ios/SignupView.swift
@@ -0,0 +1,18 @@
+//
+//  SignupView.swift
+//  aac-ios
+//
+//  Created by Joannaye on 2023/11/28.
+//
+
+import SwiftUI
+
+struct SignupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignupView()
+}

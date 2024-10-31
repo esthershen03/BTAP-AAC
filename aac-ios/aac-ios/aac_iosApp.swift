3ecb2b75477f2209749987ad7a5f5e194f50bb87
Pathak, Samiya <spathak67@gatech.edu>
2024-10-31 15:50:32 -0400

    Update aac_iosApp.swift
    Switch from CoreData to Firebase

--- aac-ios/aac-ios/aac_iosApp.swift
+++ aac-ios/aac-ios/aac_iosApp.swift
@@ -6,11 +6,15 @@
 //
 
 import SwiftUI
-import CoreData
+import Firebase
 
 @main
 struct aac_iosApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    init() {
+        FirebaseApp.configure()
+    }
 
     var body: some Scene {
         WindowGroup {

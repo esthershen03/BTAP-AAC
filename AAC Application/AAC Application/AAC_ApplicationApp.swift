f34f2827353112a1ad77d58b07750fa7eaa7055b
Pathak, Samiya <spathak67@gatech.edu>
2024-11-21 21:11:01 -0500

    Update AAC_ApplicationApp.swift

--- AAC Application/AAC Application/AAC_ApplicationApp.swift
+++ AAC Application/AAC Application/AAC_ApplicationApp.swift
@@ -6,12 +6,28 @@
 //
 
 import SwiftUI
+import CoreData
 
 @main
+@UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 struct AAC_ApplicationApp: App {
+    var appDelegate = AppDelegate()
+    let persistentContainer: NSPersistentContainer
+
+    init() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            fatalError("Could not cast UIApplication.shared.delegate to AppDelegate")
+        }
+        self.persistentContainer = appDelegate.persistentContainer
+    }
     var body: some Scene {
         WindowGroup {
-            Login()
+            if isLoggedIn {
+                Scripts()
+                    environment(\.managedObjectContext, appDelegate.persistentContainer.viewContext)
+            } else {
+                Login()
+            }
         }
     }
 }
